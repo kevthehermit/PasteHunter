ec28d24db48d9c402fbe09b0f5ea809998666dbc
recrudesce <recrudesce@gmail.com>
2017-11-08 14:38:19 +0000

    Added ES SSL support via config parameter Added store_all capability Modified data printed by ES output module

--- outputs/elastic_output.py
+++ outputs/elastic_output.py
@@ -12,9 +12,10 @@ class ElasticOutput():
         es_user = config['elastic_output']['elastic_user']
         es_pass = config['elastic_output']['elastic_pass']
         self.es_index = config['elastic_output']['elastic_index']
+        es_ssl = config['elastic_output']['elastic_ssl']
         self.test = False
         try:
-            self.es = Elasticsearch(es_host, port=es_port, http_auth=(es_user, es_pass))
+            self.es = Elasticsearch(es_host, port=es_port, http_auth=(es_user, es_pass), use_ssl=es_ssl)
             self.test = True
         except Exception:
             raise Exception('Unable to Connect') from None
@@ -24,6 +25,6 @@ class ElasticOutput():
             index_name = self.es_index
             # Consider adding date to the index
             self.es.index(index=index_name, doc_type='paste', id=paste_data['key'], body=paste_data)
-            print("Stored Paste {0}".format(paste_data['key']))
+            print("Stored Paste {0}, Matched Rule {1}".format(paste_data['key'], paste_data['YaraRule']))
         else:
             print("Elastic Search Enabled, not configured!")
