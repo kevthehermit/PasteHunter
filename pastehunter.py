4f856899d227126f8b4c2dbf64b0e9960db10807
thehermit <thehermit@pastehunter.com>
2019-02-09 20:57:02 +0000

    fix blacklist typo

--- pastehunter.py
+++ pastehunter.py
@@ -216,7 +216,7 @@ def paste_scanner():
         for post_process, post_values in conf["post_process"].items():
             if post_values["enabled"]:
                 if any(i in results for i in post_values["rule_list"]) or "ALL" in post_values["rule_list"]:
-                    if not Blacklisted and store_all:
+                    if not blacklisted and store_all:
                         logger.info("Running Post Module {0} on {1}".format(post_values["module"], paste_data["pasteid"]))
                         post_module = importlib.import_module(post_values["module"])
                         post_results = post_module.run(results,
