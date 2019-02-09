bca406507be68ecee4389a82e7353b7f629842bb
thehermit <thehermit@ide.star-labs.co.uk>
2019-02-09 19:35:17 +0000

    Add stackexchange and fix gists

---
+++ inputs/stackexchange.py
@@ -0,0 +1,80 @@
+import requests
+import math
+import logging
+from datetime import datetime
+
+# Set some logging options
+logger = logging.getLogger('pastehunter')
+logging.getLogger('requests').setLevel(logging.ERROR)
+
+# Test API Key from the docs - U4DMV*8nvpm3EOpvf69Rxw((
+# https://api.stackexchange.com/2.2/questions?key=U4DMV*8nvpm3EOpvf69Rxw((&site=stackoverflow&page=1&pagesize=100&order=desc&sort=creation&filter=default
+
+
+
+def recent_pastes(conf, input_history):
+    api_key = conf['inputs']['stackexchange']['api_key']
+    api_scrape = conf['inputs']['stackexchange']['api_scrape']
+    site_list = conf['inputs']['stackexchange']['site_list']
+    store_filter = conf['inputs']['stackexchange']['store_filter']
+    question_body_filter = '!bA1dOlliDM)pi9'
+    pagesize = 100 # Default = 30
+    headers = {'user-agent': 'PasteHunter'}
+
+    if api_key == '':
+        logger.error("No API Key configured for StackExchange Access")
+        return [], []
+
+    result_pages = []
+    history = []
+    paste_list = []
+
+    try:
+        
+        # For each of the stack sites we want to query
+        for site in site_list:
+
+            # Create the API uri
+            scrape_uri = '{0}?key={1}&site={2}&page=1&pagesize=100&order=desc&sort=creation&filter={3}'.format(api_scrape, api_key, site, store_filter)
+            # Get some pastes and convert to json
+            # Get last 'paste_limit' pastes
+            paste_list_request = requests.get(scrape_uri)
+    
+            # ToDo: Add an API rate test in here. 
+            paste_list_json = paste_list_request.json()
+            
+            logger.info("Used {0} of {1} in api quota".format(paste_list_json['quota_remaining'], paste_list_json['quota_max']))
+    
+            for question in paste_list_json['items']:
+                # Track question ids to prevent dupes
+                history.append(question['question_id'])
+                if question['question_id'] in input_history:
+                    continue
+    
+                # Create a new question dict for us to normalize
+                question_data = question
+                question_data['confname'] = "stackexchange"
+                question_data['pasteid'] = question['question_id']
+                question_data['pastesite'] = site
+                # Get the author and then trim the data we store. 
+                question_data['username'] = question['owner']['display_name']
+                del question_data['owner']
+                # Add a date field that kibana will map
+                date = datetime.utcfromtimestamp(float(question_data['creation_date'])).isoformat()
+                question_data['@timestamp'] = date
+                question_data['scrape_url'] = "{0}/{1}?key={2}&order=desc&sort=activity&site={3}&filter={4}".format(
+                    api_scrape, 
+                    question['question_id'],
+                    api_key,
+                    site,
+                    question_body_filter
+                    )
+                paste_list.append(question_data)
+            
+        
+        # Return the pastes and update history
+        return paste_list, history
+
+    except Exception as e:
+        logger.error("Unable to parse question results: {0}".format(e))
+        return paste_list, history
