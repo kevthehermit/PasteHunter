62f262b571c7e4df8caef4c27537e25aeeadc318
KevTheHermit <thehermit@pastehunter.com>
2019-04-08 17:48:47 +0000

    Stop timout on empty queue

--- pastehunter.py
+++ pastehunter.py
@@ -149,137 +149,139 @@ def paste_scanner():
     # scan the Paste
     # Store the Paste
     while True:
-        with timeout(seconds=10):
+        while not q.empty():
             paste_data = q.get()
-            # Start a timer
-            start_time = time.time()
-            logger.debug("Found New {0} paste {1}".format(paste_data['pastesite'], paste_data['pasteid']))
-            # get raw paste and hash them
-            try:
+            with timeout(seconds=10):
                 
-                # Stack questions dont have a raw endpoint
-                if ('stackexchange' in conf['inputs']) and (paste_data['pastesite'] in conf['inputs']['stackexchange']['site_list']):
-                    # The body is already included in the first request so we do not need a second call to the API. 
-                    
-                    # Unescape the code block strings in the json body. 
-                    raw_body = paste_data['body']
-                    raw_paste_data = unquote_plus(raw_body)
-                    
-                    # now remove the old body key as we dont need it any more
-                    del paste_data['body']
-                    
-                else:
-                    raw_paste_uri = paste_data['scrape_url']
-                    raw_paste_data = requests.get(raw_paste_uri).text
-                    
-            # Cover fetch site SSLErrors
-            except requests.exceptions.SSLError as e:
-                logger.error("Unable to scan raw paste : {0} - {1}".format(paste_data['pasteid'], e))
-                raw_paste_data = ""
-    
-            # Pastebin Cache
-            if raw_paste_data == "File is not ready for scraping yet. Try again in 1 minute.":
-                logger.info("Paste is still cached sleeping to try again")
-                sleep(45)
+                # Start a timer
+                start_time = time.time()
+                logger.debug("Found New {0} paste {1}".format(paste_data['pastesite'], paste_data['pasteid']))
                 # get raw paste and hash them
-                raw_paste_uri = paste_data['scrape_url']
-                # Cover fetch site SSLErrors
                 try:
-                    raw_paste_data = requests.get(raw_paste_uri).text
+                    
+                    # Stack questions dont have a raw endpoint
+                    if ('stackexchange' in conf['inputs']) and (paste_data['pastesite'] in conf['inputs']['stackexchange']['site_list']):
+                        # The body is already included in the first request so we do not need a second call to the API. 
+                        
+                        # Unescape the code block strings in the json body. 
+                        raw_body = paste_data['body']
+                        raw_paste_data = unquote_plus(raw_body)
+                        
+                        # now remove the old body key as we dont need it any more
+                        del paste_data['body']
+                        
+                    else:
+                        raw_paste_uri = paste_data['scrape_url']
+                        raw_paste_data = requests.get(raw_paste_uri).text
+                        
+                # Cover fetch site SSLErrors
                 except requests.exceptions.SSLError as e:
                     logger.error("Unable to scan raw paste : {0} - {1}".format(paste_data['pasteid'], e))
                     raw_paste_data = ""
-            # Process the paste data here
-            try:
-                # Scan with yara
-                matches = rules.match(data=raw_paste_data)
-            except Exception as e:
-                logger.error("Unable to scan raw paste : {0} - {1}".format(paste_data['pasteid'], e))
-                continue
-    
-            results = []
-            for match in matches:
-                # For keywords get the word from the matched string
-                if match.rule == 'core_keywords' or match.rule == 'custom_keywords':
-                    for s in match.strings:
-                        rule_match = s[1].lstrip('$')
-                        if rule_match not in results:
-                            results.append(rule_match)
-                    results.append(str(match.rule))
-    
-                # But a break in here for the base64. Will use it later.
-                elif match.rule.startswith('b64'):
-                    results.append(match.rule)
-    
-                # Else use the rule name
-                else:
-                    results.append(match.rule)
-    
-            # Store all OverRides other options. 
-            paste_site = paste_data['confname']
-            store_all = conf['inputs'][paste_site]['store_all']
-            # remove the confname key as its not really needed past this point
-            del paste_data['confname']
-    
-    
-            # Blacklist Check
-            # If any of the blacklist rules appear then empty the result set
-            blacklisted = False
-            if conf['yara']['blacklist'] and 'blacklist' in results:
-                results = []
-                blacklisted = True
-                logger.info("Blacklisted {0} paste {1}".format(paste_data['pastesite'], paste_data['pasteid']))
-    
-    
-            # Post Process
-    
-            # If post module is enabled and the paste has a matching rule.
-            post_results = paste_data
-            for post_process, post_values in conf["post_process"].items():
-                if post_values["enabled"]:
-                    if any(i in results for i in post_values["rule_list"]) or "ALL" in post_values["rule_list"]:
-                        if not blacklisted:
-                            logger.info("Running Post Module {0} on {1}".format(post_values["module"], paste_data["pasteid"]))
-                            post_module = importlib.import_module(post_values["module"])
-                            post_results = post_module.run(results,
-                                                            raw_paste_data,
-                                                            paste_data
-                                                            )
-    
-            # Throw everything back to paste_data for ease.
-            paste_data = post_results
-    
-    
-            # If we have a result add some meta data and send to storage
-            # If results is empty, ie no match, and store_all is True,
-            # then append "no_match" to results. This will then force output.
-    
-            if store_all is True:
-                if len(results) == 0:
-                    results.append('no_match')
-                    
-            if len(results) > 0:
-    
-                encoded_paste_data = raw_paste_data.encode('utf-8')
-                md5 = hashlib.md5(encoded_paste_data).hexdigest()
-                sha256 = hashlib.sha256(encoded_paste_data).hexdigest()
-                paste_data['MD5'] = md5
-                paste_data['SHA256'] = sha256
-                paste_data['raw_paste'] = raw_paste_data
-                paste_data['YaraRule'] = results
-                # Set the size for all pastes - This will override any size set by the source
-                paste_data['size'] = len(raw_paste_data)
-                for output in outputs:
+        
+                # Pastebin Cache
+                if raw_paste_data == "File is not ready for scraping yet. Try again in 1 minute.":
+                    logger.info("Paste is still cached sleeping to try again")
+                    sleep(45)
+                    # get raw paste and hash them
+                    raw_paste_uri = paste_data['scrape_url']
+                    # Cover fetch site SSLErrors
                     try:
-                        output.store_paste(paste_data)
-                    except Exception as e:
-                        logger.error("Unable to store {0} to {1} with error {2}".format(paste_data["pasteid"], output, e))
-            
-            end_time = time.time()
-            logger.debug("Processing Finished for {0} in {1} seconds".format(
-                paste_data["pasteid"],
-                (end_time - start_time)
-            ))
+                        raw_paste_data = requests.get(raw_paste_uri).text
+                    except requests.exceptions.SSLError as e:
+                        logger.error("Unable to scan raw paste : {0} - {1}".format(paste_data['pasteid'], e))
+                        raw_paste_data = ""
+                # Process the paste data here
+                try:
+                    # Scan with yara
+                    matches = rules.match(data=raw_paste_data)
+                except Exception as e:
+                    logger.error("Unable to scan raw paste : {0} - {1}".format(paste_data['pasteid'], e))
+                    continue
+        
+                results = []
+                for match in matches:
+                    # For keywords get the word from the matched string
+                    if match.rule == 'core_keywords' or match.rule == 'custom_keywords':
+                        for s in match.strings:
+                            rule_match = s[1].lstrip('$')
+                            if rule_match not in results:
+                                results.append(rule_match)
+                        results.append(str(match.rule))
+        
+                    # But a break in here for the base64. Will use it later.
+                    elif match.rule.startswith('b64'):
+                        results.append(match.rule)
+        
+                    # Else use the rule name
+                    else:
+                        results.append(match.rule)
+        
+                # Store all OverRides other options. 
+                paste_site = paste_data['confname']
+                store_all = conf['inputs'][paste_site]['store_all']
+                # remove the confname key as its not really needed past this point
+                del paste_data['confname']
+        
+        
+                # Blacklist Check
+                # If any of the blacklist rules appear then empty the result set
+                blacklisted = False
+                if conf['yara']['blacklist'] and 'blacklist' in results:
+                    results = []
+                    blacklisted = True
+                    logger.info("Blacklisted {0} paste {1}".format(paste_data['pastesite'], paste_data['pasteid']))
+        
+        
+                # Post Process
+        
+                # If post module is enabled and the paste has a matching rule.
+                post_results = paste_data
+                for post_process, post_values in conf["post_process"].items():
+                    if post_values["enabled"]:
+                        if any(i in results for i in post_values["rule_list"]) or "ALL" in post_values["rule_list"]:
+                            if not blacklisted:
+                                logger.info("Running Post Module {0} on {1}".format(post_values["module"], paste_data["pasteid"]))
+                                post_module = importlib.import_module(post_values["module"])
+                                post_results = post_module.run(results,
+                                                                raw_paste_data,
+                                                                paste_data
+                                                                )
+        
+                # Throw everything back to paste_data for ease.
+                paste_data = post_results
+        
+        
+                # If we have a result add some meta data and send to storage
+                # If results is empty, ie no match, and store_all is True,
+                # then append "no_match" to results. This will then force output.
+        
+                if store_all is True:
+                    if len(results) == 0:
+                        results.append('no_match')
+                        
+                if len(results) > 0:
+        
+                    encoded_paste_data = raw_paste_data.encode('utf-8')
+                    md5 = hashlib.md5(encoded_paste_data).hexdigest()
+                    sha256 = hashlib.sha256(encoded_paste_data).hexdigest()
+                    paste_data['MD5'] = md5
+                    paste_data['SHA256'] = sha256
+                    paste_data['raw_paste'] = raw_paste_data
+                    paste_data['YaraRule'] = results
+                    # Set the size for all pastes - This will override any size set by the source
+                    paste_data['size'] = len(raw_paste_data)
+                    for output in outputs:
+                        try:
+                            output.store_paste(paste_data)
+                        except Exception as e:
+                            logger.error("Unable to store {0} to {1} with error {2}".format(paste_data["pasteid"], output, e))
+                
+                end_time = time.time()
+                logger.debug("Processing Finished for {0} in {1} seconds".format(
+                    paste_data["pasteid"],
+                    (end_time - start_time)
+                ))
 
 
 
