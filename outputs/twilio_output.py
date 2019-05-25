4d07119f4ede5905e66f66893ebd9de8f821482d
KevTheHermit <kevthehermit@gmail.com>
2019-05-25 17:55:24 +0100

    Fix twilio only send on user set rule list.

--- outputs/twilio_output.py
+++ outputs/twilio_output.py
@@ -11,6 +11,7 @@ class TwilioOutput():
         self.auth_token = config['outputs']['twilio_output']['auth_token']
         self.twilio_sender = config['outputs']['twilio_output']['twilio_sender']
         self.recipient_list = config['outputs']['twilio_output']['recipient_list']
+        self.accepted_rules = config['outputs']['twilio_output']['rule_list']
         self.message_type = 'sms' # Whatsapp is still in beta on twilio.
         try:
             self.client = Client(self.account_sid, self.auth_token)
@@ -23,37 +24,43 @@ class TwilioOutput():
     def store_paste(self, paste_data):
         if self.test:
 
-            message_body = "Yara Rule {0} Found on {1}\n\r{2}".format(
-                paste_data['YaraRule'], 
-                paste_data['pastesite'], 
-                paste_data['scrape_url']
-                )
 
-            if self.message_type == 'sms':
-                for recipient in self.recipient_list:
-                    try:
-                        message = self.client.messages.create( 
-                                                    from_=self.twilio_sender,  
-                                                    body=message_body,      
-                                                    to=recipient 
-                                                )
-                        logging.debug("Sent twilio message with ID: {0}".format(message.sid))
-                    except Exception as e:
-                        logging.error(e)
+            send = ('all' in self.accepted_rules)
 
-            elif self.message_type == 'whatsapp':
-                for recipient in self.recipient_list:
-                    try:
-                        message = self.client.messages.create( 
-                                                    from_='whatsapp:{0}'.format(self.twilio_sender),  
-                                                    body=message_body,      
-                                                    to='whatsapp:{0}'.format(recipient) 
-                                                )
-                        logging.debug("Sent twilio message with ID: {0}".format(message.sid))
-                    except Exception as e:
-                        logging.error(e)
-            else:
-                logging.error("No Valid twilio message type found")
+            for rule in self.accepted_rules:
+                if rule in paste_data['YaraRule']:
+                    send = True
 
-        else:
-            logging.error("Unable to send twilio message")
+            if send:
+                message_body = "Yara Rule {0} Found on {1}\n\r{2}".format(
+                    paste_data['YaraRule'], 
+                    paste_data['pastesite'], 
+                    paste_data['scrape_url']
+                    )
+
+                print("Sending Twilio Message")
+                if self.message_type == 'sms':
+                    for recipient in self.recipient_list:
+                        try:
+                            message = self.client.messages.create( 
+                                                        from_=self.twilio_sender,  
+                                                        body=message_body,      
+                                                        to=recipient 
+                                                    )
+                            logging.debug("Sent twilio message with ID: {0}".format(message.sid))
+                        except Exception as e:
+                            logging.error(e)
+
+                elif self.message_type == 'whatsapp':
+                    for recipient in self.recipient_list:
+                        try:
+                            message = self.client.messages.create( 
+                                                        from_='whatsapp:{0}'.format(self.twilio_sender),  
+                                                        body=message_body,      
+                                                        to='whatsapp:{0}'.format(recipient) 
+                                                    )
+                            logging.debug("Sent twilio message with ID: {0}".format(message.sid))
+                        except Exception as e:
+                            logging.error(e)
+                else:
+                    logging.error("No Valid twilio message type found")
